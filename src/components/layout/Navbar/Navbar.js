36c252f2056df18c2206607d259f073b0ef35f7f
Gustavo Santos <gustavo.santos@MAISVALOR.COM>
2023-06-22 16:00:55 -0300

    refactor: ajuste no responsivo da navbar

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -3,8 +3,8 @@ import { Nav } from "../Nav/Nav"
 
 export function Navbar() {
     return (
-        <div className="w-full h-24 flex flex-col items-center lg:flex lg:flex-row lg:place-content-around bg-[#255C99]">
-            <Logo style={`text-sky-50 my-3`}/>
+        <div className="w-full h-32 flex flex-col items-center lg:h-16 lg:flex lg:flex-row lg:place-content-around bg-[#255C99]">
+            <Logo style={`text-sky-50 my-5`}/>
             <Nav />
         </div>
     )
